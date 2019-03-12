4e5881e594ef4f3a2a2eb26558e36645cfcaa96c
JerryWu <wujerry116@163.com>
2019-03-13 00:55:49 +0800

    add sort asset

--- lib/src/ui/page/photo_main_page.dart
+++ lib/src/ui/page/photo_main_page.dart
@@ -171,6 +171,7 @@ class _PhotoMainPageState extends State<PhotoMainPage>
     galleryPathList.addAll(widget.photoList);
     this.list.clear();
     var assetList = await galleryPathList[0].assetList;
+    _sortAssetList(assetList);
     this.list.addAll(assetList);
     setState(() {
       _isInit = true;
@@ -203,6 +204,7 @@ class _PhotoMainPageState extends State<PhotoMainPage>
 
     if (pathList.isNotEmpty) {
       imageList = await pathList[0].assetList;
+      _sortAssetList(imageList);
       _currentPath = pathList[0];
     }
 
@@ -219,6 +221,10 @@ class _PhotoMainPageState extends State<PhotoMainPage>
     setState(() {
       _isInit = true;
     });
+  }
+
+  void _sortAssetList(List<AssetEntity> assetList) {
+    options?.sortDelegate?.assetDelegate?.sort(assetList);
   }
 
   Widget _buildBody() {
@@ -335,6 +341,7 @@ class _PhotoMainPageState extends State<PhotoMainPage>
     _currentPath = value;
 
     _currentPath.assetList.then((v) {
+      _sortAssetList(v);
       list.clear();
       list.addAll(v);
       scrollController.jumpTo(0.0);
